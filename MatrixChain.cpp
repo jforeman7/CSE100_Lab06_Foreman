9dd461e3ec48829e20dcef3a8067ed7942179d25
jforeman7 <31979772+jforeman7@users.noreply.github.com>
2017-10-25 17:08:51 -0700

    Added PrintOptimalParens.

--- MatrixChain.cpp
+++ MatrixChain.cpp
@@ -4,3 +4,27 @@
 // 25 Oct 2017
 
 #include <iostream>
+
+using namespace std;
+
+// Pseudocode found on page 377.
+void PrintOptimalParens(int i, int j, int bracket, int *name)
+{
+	if (i == j)
+	{
+		cout << "A" << (i - 1);
+		return;
+	}
+	
+	else
+	{
+		cout << "(";
+		
+		int jp = name + i * bracket + j;
+		
+		PrintOptimalParens(i, *jp, bracket, name);
+		PrintOptimalParens(*jp + 1, j, bracket, name);
+		
+		cout << ")";
+	}
+}
