84bc555e7d355b99b4d2295b1d089f735c7eb8c7
jforeman7 <31979772+jforeman7@users.noreply.github.com>
2017-10-25 17:40:55 -0700

    Implemented MatrixChainOrder and main.

--- MatrixChain.cpp
+++ MatrixChain.cpp
@@ -4,18 +4,21 @@
 // 25 Oct 2017
 
 #include <iostream>
+#include <limits> // To set a variable to "infinity".
 
 using namespace std;
 
 // Pseudocode found on page 377.
 void PrintOptimalParens(int i, int j, int bracket, int *name)
 {
+	// Print out the name of the Matrix.
 	if (i == j)
 	{
 		cout << "A" << (i - 1);
 		return;
 	}
 	
+	// Place the parenthesis.
 	else
 	{
 		cout << "(";
@@ -27,10 +30,72 @@ void PrintOptimalParens(int i, int j, int bracket, int *name)
 		
 		cout << ")";
 	}
-}
+} // End function.
 
 // Pseudocode found on page 375.
 void MatrixChainOrder(int p[], int pSize)
 {
-		
+	// Allocate memory for the two dimensional matrix arrays.
+	int m[n][n];
+	int s[n][n];
+	
+	// Initialize the diagonal entries in matrix 'm' to zero.
+	for (int i = 0; i < n; i++)
+		m[i][i] = 0;;
+	
+	
+	// Extra iterator variable.
+	int j;
+	
+	// Temp variable for the matrix chain multiplication.
+	int q;
+	
+	for (int l = 2; l < n; l++)
+	{
+		for (int i = 1; i < n - l + 1; i++)
+		{
+			j = i + l - 1;
+			
+			// Set to "infinity".
+			m[i][j] = numeric_limits<int>::max();
+			
+			for (int k = i; k <= j - 1; k++)
+			{
+				q = m[i][k] + m[k + 1][j] + (p[i - 1] * p[k] * p[j]);
+				
+				if (q < m[i][j])
+				{
+					m[i][j] = q;
+					s[i][j] = k;
+				} // End if.
+			} // End k loop.	
+		} // End i loop.
+	} // End l loop.
+	
+	// Print out the result.
+	cout << m[1][n - 1] << endl;
+	
+	// Print out the optimal chain multiplication.
+	PrintOptimalParens(1, n-1, n, int *s);
+	cout << endl;
+	
+} // End function.
+
+int main()
+{
+	// Get the size of the array.
+	int n;
+	cin >> n;
+	
+	// Instantiate the array.
+	int *A = new int[n];
+	
+	// Input into the array.
+	for (int i = 0; i < n; i++)
+		cin >> A[i];
+	
+	// Begin the Matrix Chain Multiplication.
+	MatrixChainOrder(A, n);
+	
+	return 0;
 }
